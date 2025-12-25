2d9d882a96fbf73ad6c5be6745af7a0424578046
Ivan Campelo <ivancampelo@MacBook-Air-de-Ivan.local>
2025-12-24 22:17:07 -0300

    feat: ci/cd

--- tests.py
+++ tests.py
@@ -572,3 +572,4 @@ async def test_rescue_invalid_initial_lack():
     
     assert exc_info.value.status_code == 400
     assert exc_info.value.detail == "Carência inicial de resgate de 60 dias não foi cumprida."
+    
