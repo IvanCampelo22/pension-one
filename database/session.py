ccf9b0fbeb5219dc067bbbca9453ba5ea21225d4
Ivan Campelo <ivancampelo@MacBook-Air-de-Ivan.local>
2025-12-25 21:03:09 -0300

    feat: add enviroment

--- database/session.py
+++ database/session.py
@@ -3,10 +3,28 @@ from sqlalchemy.orm import sessionmaker, declarative_base
 
 from api.v1.core.config import settings
 
-SQLALCHEMY_DATABASE_URL = f"postgresql+asyncpg://{settings.DB_USER}:{settings.DB_PASSWORD}@{settings.DB_HOST}/{settings.DB_NAME}?ssl=require"
+DATABASE_URL = (
+    f"postgresql+asyncpg://{settings.DB_USER}"
+    f"@{settings.DB_HOST}:{settings.DB_PORT}/{settings.DB_NAME}"
+)
 
-engine = create_async_engine(SQLALCHEMY_DATABASE_URL, future=True, echo=True, pool_pre_ping=True,)
-AsyncSessionLocal = sessionmaker(bind=engine, expire_on_commit=False, class_=AsyncSession, future=True)
+engine = create_async_engine(
+    DATABASE_URL,
+    future=True,
+    echo=True,
+    pool_pre_ping=True,
+    connect_args={
+        "password": settings.DB_PASSWORD,
+        "ssl": "require",
+    },
+)
+
+AsyncSessionLocal = sessionmaker(
+    bind=engine,
+    expire_on_commit=False,
+    class_=AsyncSession,
+    future=True,
+)
 
 Base = declarative_base()
 
@@ -14,9 +32,9 @@ async def get_async_session():
     async with AsyncSessionLocal() as session:
         try:
             yield session
-        except Exception as e:
+        except Exception:
             await session.rollback()
-            raise e
+            raise
         finally:
             await session.close()
 
@@ -27,9 +45,9 @@ def async_session(func):
                 result = await func(session, *args, **kwargs)
                 await session.commit()
                 return result
-            except Exception as e:
+            except Exception:
                 await session.rollback()
-                raise e
+                raise
             finally:
                 await session.close()
     return wrapper
