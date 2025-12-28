f48fa7ed9ca792a6464824ed185f873664bebc69
Ivan Campelo <ivancampelo@MacBook-Air-de-Ivan.local>
2025-12-28 19:59:15 -0300

    fix: remove .env and use enviroment

--- api/v1/core/config.py
+++ api/v1/core/config.py
@@ -4,7 +4,8 @@ from urllib.parse import quote_plus
 class Settings:
     DB_NAME = os.environ["DB_NAME"]
     DB_USER = os.environ["DB_USER"]
-    DB_PASSWORD = quote_plus(os.environ["DB_PASSWORD"])
+    _DB_PASSWORD_RAW = os.environ["DB_PASSWORD"]
+    DB_PASSWORD = quote_plus(_DB_PASSWORD_RAW)
     DB_HOST = os.environ["DB_HOST"]
     DB_PORT = int(os.environ.get("DB_PORT") or 5432)
 
