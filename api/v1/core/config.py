a850e787fc4da0d54af7097eb954d39fd34fb76c
Ivan Campelo <ivancampelo@MacBook-Air-de-Ivan.local>
2025-12-28 22:08:07 -0300

    fix: remove .env and use enviroment

--- api/v1/core/config.py
+++ api/v1/core/config.py
@@ -5,7 +5,7 @@ class Settings:
     DB_NAME = os.environ["DB_NAME"]
     DB_USER = os.environ["DB_USER"]
     _DB_PASSWORD_RAW = os.environ["DB_PASSWORD"]
-    DB_PASSWORD = 'K7!vQ3^mP9#tL2$$zX5&h'
+    DB_PASSWORD = quote_plus(_DB_PASSWORD_RAW)
     DB_HOST = os.environ["DB_HOST"]
     DB_PORT = int(os.environ.get("DB_PORT") or 5432)
 
